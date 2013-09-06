4558a2c4c9f0de36ffebefe39b9317f0b92b435a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 18:34:46 -0500

    moves facebook js out of main js folder into subfolder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :created_projects, class_name: "Project", foreign_key: :owner_id
   has_many :donations
-  has_many :donated_projects, class_name: "Project", through: :donations
+  has_many :donated_projects, through: :donations, source: :project
 
   validates_presence_of :email
 

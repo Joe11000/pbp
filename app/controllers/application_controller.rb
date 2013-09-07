3031118be5778036dba1704a45a0ea6bbf18a35f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 19:37:13 -0500

    add cancan authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include ApplicationHelper
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url
+  end
+
+private 
+	helper_method :current_user
 end

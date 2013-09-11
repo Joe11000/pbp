17bc11e4e8b6f032ecd2e5b092b828bc1ba9a86e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 09:38:15 -0500

    stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,6 @@ class EventsController < ApplicationController
       @project = Project.find(params[:project_id]) 
       @events = @project.events
     if request.xhr?
-      # @events = @project.get_events_for_day(params[:date])
       render json: @events
     else
       render :index
